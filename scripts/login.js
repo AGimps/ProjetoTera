d056ab5e34f707dff82e6a5b0d8c6dc0b3b08e11
AGimps <arielgimpel@hotmail.com>
2022-06-04 20:56:41 -0300

    Versão 1.2: Adicionado estilização básica, página de grupos e interface do site. Falta: adicionar página de amigos e favoritos, terminar estilização.

--- scripts/login.js
+++ scripts/login.js
@@ -2,6 +2,18 @@ function loginUsuario(){
     var usuario = document.getElementById("txtLogin").value;
     return usuario;
 
+}
+
+function funcConfirmar() {
+  if (confirm("Deseja confirmar o cadastro?") == true) {
+    window.alert("Cadastro completo");
+    return true;
+} else {
+  window.alert("Cadastro cancelado");
+    return false;
+}
+}
+
   /*   let divMensagem = document.querySelector("#divMensagem");
   
     //alterando o atributo "style" para exibir este elemento
@@ -9,7 +21,7 @@ function loginUsuario(){
     
     let tituloMensagem = document.querySelector("#tituloMensagem");
     tituloMensagem.innerHTML = 'Seja bem vindo(a) ' + usuario; */
-}
+
 
 /* document.addEventListener("DOMContentLoaded", function(){
   
